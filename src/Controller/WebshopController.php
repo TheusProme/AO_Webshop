bec674832ad61d4448a63d96eb7f6509ceaf4a6b
Theus de Zeeuw <theusdezeeuw@outlook.com>
2020-06-02 11:44:56 +0200

    Update MPA

---
+++ src/Controller/WebshopController.php
@@ -0,0 +1,30 @@
+<?php
+
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class WebshopController extends AbstractController {
+/*-----------------------------------------------------------Display: ------------------------------------------------------------  */
+    
+    public function Welcome()
+    {
+        $number = random_int(0, 10);
+
+        return $this->render('Home.html.twig', [
+            'Number' => $number,
+        ]);
+    }
+
+    public function Product()
+    {
+
+        return $this->render('Product.html.twig', [
+        ]);
+    }
+
+/*-----------------------------------------------------------Display: ------------------------------------------------------------  */
+
+}
