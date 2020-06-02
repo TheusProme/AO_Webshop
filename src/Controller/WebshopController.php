f86ff3a2aac84f9dd8586c68e4a55bb799ff9ef5
Theus de Zeeuw <theusdezeeuw@outlook.com>
2020-06-02 12:23:24 +0200

    Page Product

--- src/Controller/WebshopController.php
+++ src/Controller/WebshopController.php
@@ -20,11 +20,40 @@ class WebshopController extends AbstractController {
 
     public function Product()
     {
-
+        
+        // $Get_Products = $this->get_products();
+        
         return $this->render('Product.html.twig', [
+            // 'Products' => $Get_Products,
         ]);
     }
 
 /*----------------------------------------------------------- Functions: ------------------------------------------------------------  */
+    
+    public function databaseconnection(){
+
+    $dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "mysql";
+    $dbName = "AO_Webshop";
+
+    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+    return $conn;
+    }
+
+    public function get_products(){
+        $products = [];
+        $conn = $this->databaseconnection();
+        $sql = "SELECT * FROM Products";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+                $products[] = $row;
+        }
+    }
+    return $products;
+    }
 
 }
